277855a245014ab4667660efaa5ced79c82ed299
Philip Vallone <vallonephilip@gmail.com>
2013-07-06 09:43:31 +0000

    put index.html in quotes

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var data = Buffer.toString(fs.readFileSync(index.html));
+var data = buffer.toString(fs.readFileSync("index.html~"));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
