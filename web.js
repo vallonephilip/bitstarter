7e5fb0b53ecaa41cb7ddd8d9fb146b08856b0403
Philip Vallone <vallonephilip@gmail.com>
2013-07-06 09:54:38 +0000

    forgot to delete a parentheses

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var data = fs.readFileSync("index.html~"));
+var data = fs.readFileSync("index.html~");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
